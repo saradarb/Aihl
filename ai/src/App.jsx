e1c4a474a722f13c7253a2a897f8d73dd8818388
sara <saradernouni@gmail.com>
2025-07-15 10:58:21 -0400

    Projectset

---
+++ ai/src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Mainpage from './components/Mainpage'
+
+
+
+
+
+function App() {
+  
+
+
+  return (
+    <>
+    <p>Hello</p>
+    <Mainpage />
+
+    </>
+  )
+}
+
+export default App
---
+++ ai/src/components/Mainpage.jsx
@@ -0,0 +1,15 @@
+import './Mainpage.css'
+
+function Mainpage() {
+  
+
+
+  return (
+    <>
+    <p>Hello, hello, hello, hello, hello</p>
+
+    </>
+  )
+}
+
+export default Mainpage
