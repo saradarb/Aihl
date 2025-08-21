770097a5b611af29a0762885f6a9934ed03c1c82
sara <saradernouni@gmail.com>
2025-08-21 19:07:10 -0400

    test 2

--- ai/src/App.jsx
+++ ai/src/App.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './style.css';
-import Lobby from './components/Lobby';
-import AiChat from './components/AiChat';
-import Price from './components/Price/Price';
-import Help from './components/Help/Help';
-import Settings from './components/Settings/Settings';
-
-
-
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom";
+import "./style.css";
+import Lobby from "./components/Lobby";
+import AiChat from "./components/AiChat";
+import Price from "./components/Price/Price";
+import Help from "./components/Help/Help";
+import Settings from "./components/Settings/Settings";
 
 function App() {
   // hi
   return (
-    <Router>
-      <Routes>
-        <Route path="/Aihl" element={<Lobby />} />
-        <Route path="/Aihl/chat" element={<AiChat />} />
-        <Route path="/Aihl/Price" element={<Price />} />
-        <Route path="/Aihl/Help" element={<Help />} />
-        <Route path="/Aihl/Settings" element={<Settings />} />
-      </Routes>
-    </Router>
+    <HashRouter>
+      <Router>
+        <Routes>
+          <Route path="/Aihl" element={<Lobby />} />
+          <Route path="/Aihl/chat" element={<AiChat />} />
+          <Route path="/Aihl/Price" element={<Price />} />
+          <Route path="/Aihl/Help" element={<Help />} />
+          <Route path="/Aihl/Settings" element={<Settings />} />
+        </Routes>
+      </Router>
+    </HashRouter>
   );
 }
 
